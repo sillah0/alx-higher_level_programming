f70efa2f736227c38bc8481b4c159404a3455ade
sillah0 <siladennis624@gmail.com>
2024-01-30 00:33:12 +0300

    a rectangle with width and height set to a private instance

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
+class Rectangle:
+    """the class created"""
+
+    def __init__(self, width=0, height=0):
+
+        """Initialize width and height to they are set to 0
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieving the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the value and check if value is an int or
+        less than 0"""
+        if not isinstance(value, int):
+            raise TypeError("wisth must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value

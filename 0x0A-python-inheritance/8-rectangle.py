8e048b49eefdd15a1e3158da2c05c4cd6ed26c74
sillah0 <siladennis624@gmail.com>
2024-02-22 00:52:38 +0300

    fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-"""class BaseGeometry"""
-
 
 class BaseGeometry:
 
@@ -20,8 +18,22 @@ class BaseGeometry:
         Args:
         name (str) - name of value
         value (int) - integer
+
         """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greator than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A class inherited from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """
+        instantiation
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator = ('width', width)
+        self.integer_validator = ('height', height)

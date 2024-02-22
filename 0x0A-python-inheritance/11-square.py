01c50afca0d994a72986b6c2ef983f78c255c7a4
sillah0 <siladennis624@gmail.com>
2024-02-22 11:45:03 +0300

    fix error

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""A model containing Rectangle class"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+
+    """class Square that inherits from Rectangle class"""
+
+    def __init__(self, size):
+        """
+        Initializes size
+        Args:
+            size (int): size is private and a positive integer
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """returns area of the square"""
+
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the square description
+        [Square] <width>/<height>
+        """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)

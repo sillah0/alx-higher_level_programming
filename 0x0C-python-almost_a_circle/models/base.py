60fda8c809e897a3e34d0a86eaa2c8b2325ac48b
sillah0 <siladennis624@gmail.com>
2024-03-02 02:17:58 +0300

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """this is a class Base"""
-
+import json
 
 class Base:
     """class Base with a private attribute __nb_objects"""
@@ -17,3 +17,11 @@ class Base:
         else:
             Base.__nb__objects += 1
             self.id = Base.__nb__objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)

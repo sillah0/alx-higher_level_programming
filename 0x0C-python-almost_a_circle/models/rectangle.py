85c6139ff864125d82942d8799e49f13b54bd395
sillah0 <siladennis624@gmail.com>
2024-03-01 23:55:37 +0300

    args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -118,3 +118,15 @@ class Rectangle(Base):
         """return a string to be printed """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} " \
             f"- {self.width}/{self.height}"
+
+    def update(self, *args):
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]

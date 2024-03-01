5d3eb317198cfec246963f0c336a2297a882676e
sillah0 <siladennis624@gmail.com>
2024-03-02 01:34:12 +0300

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,20 @@ class Square(Rectangle):
         """
         self.width = value
         self.heignt = value
+
+    def update(self, *args, **kwargs):
+        """asigning arguments to each atrri"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)

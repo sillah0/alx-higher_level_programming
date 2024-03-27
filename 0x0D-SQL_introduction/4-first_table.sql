c8c9a5cbaaa170394e2f0191ce7e2105bfa1356c
sillah0 <siladennis624@gmail.com>
2024-03-28 00:27:48 +0300

    create first table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Create a table first_table in current database
+-- If the table first_table already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));

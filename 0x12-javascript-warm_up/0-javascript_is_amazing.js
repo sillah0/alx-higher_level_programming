22178245062d77dfa34aa2898d50a975555e47ff
sillah0 <siladennis624@gmail.com>
2024-04-23 00:19:55 +0300

    js is amazing

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 /* a constant variable to print javascript is amazing */
+
 const myVar = 'Javascript is amazing ';
 console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* printing 3 lines */
+
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}

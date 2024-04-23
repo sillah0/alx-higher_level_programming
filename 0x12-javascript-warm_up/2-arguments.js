68e5d9842880b292dae21b16844d0c2f6f927294
sillah0 <siladennis624@gmail.com>
2024-04-23 17:59:33 +0300

    process argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const numargs = process.argv.slice(2);
+if (numargs === 0) {
+  console.log('No argument');
+} else if (numargs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}

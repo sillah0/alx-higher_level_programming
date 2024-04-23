24b15574dd5f2c6f41b2cad90df072758219969f
sillah0 <siladennis624@gmail.com>
2024-04-23 17:05:48 +0300

    fixed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-/* printing 3 lines */
-
 const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < languages.length; i++) {
   console.log(languages[i]);

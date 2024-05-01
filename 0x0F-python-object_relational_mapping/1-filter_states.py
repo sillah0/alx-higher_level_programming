8c5074302a171c5931875c958062377ee8b24d6f
sillah0 <siladennis624@gmail.com>
2024-05-01 12:29:45 +0300

    fix errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,17 +8,14 @@ from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           password=argv[2], db=argv[3])
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \
+                    'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
 
-cursor = conn.cursor()
+    for row in rows:
+        print(row)
 
-cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \
-    'N%' ORDER BY id ASC")
-
-rows = cursor.fetchall()
-
-for row in rows:
-    print(row)
-
-cursor.close()
-conn.close()
+    cursor.close()
+    conn.close()

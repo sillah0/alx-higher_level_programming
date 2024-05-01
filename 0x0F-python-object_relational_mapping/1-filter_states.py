7d643a8d31c471af1988b0b94cf4fb47faa526eb
sillah0 <siladennis624@gmail.com>
2024-05-01 12:25:54 +0300

    fix errors

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ name starting with 'N' from hbtn_0e_0_usa
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            password=argv[2], db=argv[3])
 

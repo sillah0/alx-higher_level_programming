91fbdd5596ba34d2011e9290eaa3cb151e4aa598
sillah0 <siladennis624@gmail.com>
2024-06-19 10:16:20 +0300

    read me

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+const filepath = process.argv[2];
+
+fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(data);
+    }
+});

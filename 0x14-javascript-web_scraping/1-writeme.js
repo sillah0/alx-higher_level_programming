55db9f376dfaf8f0f96a46240e3bce359ac2c174
sillah0 <siladennis624@gmail.com>
2024-06-19 14:49:09 +0300

    fix

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filepath, content, 'utf-8', (err) => {
+    if (err) {
+        console.error(err);
+    }    
+});
